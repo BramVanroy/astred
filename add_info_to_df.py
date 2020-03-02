8893b5c8514d739d55727a28ec1c1cb2ddcf033a
BramVanroy <Bram.Vanroy@UGent.be>
2020-03-02 12:52:26 +0100

    add multiprocessing and no_cuda

--- add_info_to_df.py
+++ add_info_to_df.py
@@ -1,15 +1,14 @@
 from functools import partial
+from multiprocessing.pool import Pool
 
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-from sac import SAC
-from astred import ASTrED, GenericTree
-from sac.utils import load_nlp
+from sac import ASTrED
 
 
-def process_astred_cross(method, r):
+def process_astred_cross(r, use_gpu=True):
     # if the column n_src_sents and n_tgt_sents exists, they must be 1,
     # otherwise skip. A segment that contains more than one sentence is hard
     # to parse so we don't want those
@@ -18,14 +17,31 @@ def process_astred_cross(method, r):
             return None
 
     # res is a dict with astred values and n_avg_tokens
-    sac = SAC(**r)
-    astred = ASTrED()
-    res = astred.calculate(r['src_segment'], r['tgt_segment'], r['alignments'], method=method, verbose=0, draw=False)
+    astred = ASTrED(r['src_segment'], r['tgt_segment'], r['alignments'], use_gpu=use_gpu)
+    astred_mwe = ASTrED(r['src_segment'], r['tgt_segment'], r['alignments'],
+                        group_mwe=True,
+                        use_gpu=use_gpu)
+    res = {
+        'cross': astred.cross,
+        'seq_cross': astred.seq_cross,
+        'seq_cross_mwe': astred_mwe.seq_cross,
+        'sac_cross': astred.sac_cross,
+        'sac_cross_mwe': astred_mwe.sac_cross,
+        'n_src_tokens': astred.n_src_tokens,
+        'n_tgt_tokens': astred.n_tgt_tokens,
+        'n_null_aligns': astred.n_null_aligns,
+        'ted': astred.ted,  # regular tree edit distance
+        'astred': astred.astred  # tree edit distance using the merged trees
+    }
 
-    res['cross'] = sac.cross
-    res['seq_cross'] = sac.seq_cross
-    res['sac_cross'] = sac.sac_cross
-    res['n_null_aligns'] = sac.n_null_aligns
+    for change_type, change_d in astred.label_changes.items():
+        # default has only one value, whereas avg_token and overlap
+        # have a value for src and tgt
+        if isinstance(change_d, dict):
+            for direction, n_changes in change_d.items():
+                res[f"{change_type}_changes_{direction}"] = n_changes
+        else:
+            res[f"{change_type}_changes"] = change_d
 
     return res
 
@@ -48,44 +64,60 @@ def fill_nan(row_list):
     return row_list
 
 
-def main(fin, fout, method):
+def mapable_df_process(func, df):
+    return df.apply(func, axis=1)
+
+
+def main(args):
     """
     :param fin: input file dataframe
     :param fout: output file
-    :param method: method(s) to use
     :return:
     """
-    df = pd.read_csv(fin, sep='\t')
-    GenericTree.init_nlp('en', load_nlp('en'))
-    GenericTree.init_nlp('nl', load_nlp('nl'))
-    tqdm.pandas()
+    df = pd.read_csv(args.fin, sep='\t')
 
-    # astred and cross
-    print('ADDING CROSS AND ASTRED')
-    partial_func = partial(process_astred_cross, method)
+    gpu_partial_process = partial(process_astred_cross, use_gpu=not args.no_cuda)
 
-    # dirty looking way of fast processing
-    # as taken from https://stackoverflow.com/a/60056244/1150683
-    row_list = df.progress_apply(partial_func, axis=1)
+    if args.j > 1:
+        splits = np.array_split(df, args.splits)
+        partial_func = partial(mapable_df_process, gpu_partial_process)
+        with Pool(processes=cargs.j) as pool:
+            row_list = pd.concat(tqdm(pool.imap(partial_func, splits),
+                                      total=args.splits,
+                                      unit='split'))
+    else:
+        tqdm.pandas(unit='row')
+        row_list = df.progress_apply(gpu_partial_process, axis=1)
+
     row_list = fill_nan(row_list)
-
     data_f = pd.DataFrame(row_list)
-
     result = pd.concat([df, data_f], axis=1, sort=False)
-    result.to_csv(fout, sep='\t', index=False)
+    result.to_csv(cargs.fout, sep='\t', index=False)
 
 
 if __name__ == '__main__':
     import argparse
 
-    cparser = argparse.ArgumentParser(description="Adds values to existing dataframe, particularly seq_alignments,"
-                                                  " seq_cross, astred, and so on")
+    cparser = argparse.ArgumentParser(
+        description="Adds cross-related values to an existing DataFrame. DataFrame must contain"
+                    " columns 'src_segment', 'tgt_segment' and 'alignments'")
 
     cparser.add_argument('fin', help='Path to input file')
     cparser.add_argument('fout', help='Path to output file')
-    cparser.add_argument('-m', '--method', nargs='+', choices=['default', 'token_avg', 'overlap'], default='default',
-                         help='How to calculate the number of label changes.')
+    cparser.add_argument('-j', help='Processes to use', type=int, default=1)
+    cparser.add_argument('--splits', help='Number of splits to divide the dataset in', type=int,
+                         default=1)
+    cparser.add_argument('--no_cuda', help="Disable CUDA. Useful when using multiple processes,"
+                                           " otherwise the multiple processes' models might not"
+                                           " fit on GPU",
+                         action='store_true',
+                         default=False)
     cargs = cparser.parse_args()
 
-    main(**vars(cargs))
-    print(GenericTree.NLPS)
+    if cargs.splits < cargs.j:
+        raise ValueError(
+            "The number of splits is smaller than the number of processes (-j), which doesn't"
+            " make sense. Please ensure that the number of splits is greater than or equal"
+            " to the number of processes")
+
+    main(cargs)
