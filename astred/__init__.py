06eb74c69385a52ebb8bc21383f1edcf17024aea
Bram Vanroy <Bram.Vanroy@UGent.be>
2021-04-01 10:36:57 +0200

    default logging
    always a hassle with Python....

--- astred/__init__.py
+++ astred/__init__.py
@@ -4,3 +4,18 @@ from .sentence import Sentence
 from .span import NullSpan, Span
 from .tree import Tree
 from .word import Null, Word
+
+import logging
+
+
+def set_logger():
+    # Do not needlessly expose variables
+    logger = logging.getLogger("astred")
+    logger.setLevel(logging.INFO)
+    sh = logging.StreamHandler()
+    sh.setFormatter(logging.Formatter(fmt="%(asctime)s - [%(levelname)s]: %(message)s",
+                                      datefmt="%d-%b %H:%M:%S"))
+    logger.addHandler(sh)
+
+
+set_logger()
